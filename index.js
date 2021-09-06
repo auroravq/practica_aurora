d30dc571861160feca0661b1bf28af69804b0e83
auroravq <89799379+auroravq@users.noreply.github.com>
2021-09-06 11:42:19 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+function getIMC(){
+var peso=parseFloat((document.getElementById("peso");
+var altura=parseFloat((document.getElementById("altura");
+var IMC=peso/(altura*altura);
+  document.getElementById("IMC").innerHTML=IMC;
+ }
